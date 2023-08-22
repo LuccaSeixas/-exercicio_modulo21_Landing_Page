e60d67e295e6944914e0bc10adb9bca64d07ef54
Lucca Seixas <luccaseixas@outlook.com>
2023-08-21 23:38:49 -0300

    adicionado faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
@@ -9,10 +10,20 @@ document.addEventListener('DOMContentLoaded', function(){
             aba.classList.add('shows__list--is-active')
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
-
         })
     }
+
+    for (let i=0; i < buttons.length; i++) {
+        questions[i].addEventListener('click', abreOuFecha);
+    }
 })
+
+function abreOuFecha(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -21,6 +32,8 @@ function removeBotaoAtivo(){
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
+
+
 
 function escondeTodasAbas () {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
